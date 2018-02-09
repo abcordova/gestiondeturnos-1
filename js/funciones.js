7ea9146dd364aa9935cfdb167f33003b446e7e46
Matias <mepvdeluxe@gmail.com>
2018-02-09 07:40:41 -0300

    Funciones en jquery

---
+++ js/funciones.js
@@ -0,0 +1,49 @@
+function agregaform (datos){
+
+	d=datos.split('||');
+
+	$('#ClienId').val(d[0]);
+	$('#ClienNombre').val(d[1]);
+	$('#ClienApellido').val(d[2]);
+	$('#ClienTelefono').val(d[3]);
+	$('#ClienEmail').val(d[4]);
+	$('#ClienDireccion').val(d[5]);
+	
+}
+
+function updateCliente (){
+
+	id=$('#ClienId').val();
+	nombre=$('#ClienNombre').val();
+	apellido=$('#ClienApellido').val();
+	email=$('#ClienEmail').val();
+	telefono=$('#ClienTelefono').val();
+	direccion=$('#ClienDireccion').val();
+
+
+	cadena = "id=" + id +
+			 "&nombre=" + nombre +
+			 "&apellido="+ apellido +
+			 "&email=" + email +
+			 "&telefono=" + telefono +
+			 "&direccion=" + direccion;
+
+
+
+	$.ajax({
+		type:"POST",
+		url:"updateCliente.php",
+		data: cadena,
+		success:function(r){
+			if(r){
+				alertify.success("Actualizacion Correcta :)");
+				$("#tabla").load(" #tabla");		
+			}else{
+				alertify.error("NO se puedo Actualizar :(");
+			}
+
+		}
+
+	})			 
+
+}
